f88e230416fb14c4527aefe36b2ff6486d362d2b
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-08-03 21:45:07 +0200

    backend

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import PostModel from "../Models/PostModel.js";
+import UserModel from "../Models/UserModel.js";
 
 // YENIDEN OLUSTURMA
 export const createPost = async (req,res)=>{
@@ -70,3 +71,43 @@ export const deletePost = async (req,res)=>{
     }
 }
 // begenme
+
+
+
+// Get Timeline Posts
+export const getTimelinePosts = async (req,res) =>{
+    const userId =req.params.id
+    
+    try {
+        const currentUserPosts = await PostModel.find({userId :userId})
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match : {
+                    _id:new mongoose.Types.ObjectId(userId)
+                }
+                
+            },
+            {
+                $lookup:{
+                    from:'posts',
+                    localField:'following',
+                    foreignField:'userId',
+                    as:'followingPosts'
+                }
+            },
+            {
+                $project:{
+                    followingPosts:1,
+                    _id:0
+                }
+            }
+            
+        ])
+        res.status(200).json(currentUserPosts.concat(...followingPosts[0].followingPosts).sort
+        ((a,b)=>{ 
+            return b.createdAt - a.createdAt
+        }))
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, deletePost, getPost, updatePost } from "../Controllers/PostController.js";
+import { createPost, deletePost, getPost, getTimelinePosts, updatePost } from "../Controllers/PostController.js";
 
 const router = express.Router()
 
@@ -9,4 +9,5 @@ router.post('/' ,createPost)
 router.get('/:id', getPost)
 router.put('/:id',updatePost)
 router.delete('/:id',deletePost)
+router.get('/:id/timeline',getTimelinePosts)
 export default router
